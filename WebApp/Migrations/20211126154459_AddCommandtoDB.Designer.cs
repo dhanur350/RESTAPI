a8e4f026731a3ebb2754247ec54fb7e013fba96e
dhanur350 <dhanur297@gmail.com>
2022-07-18 16:21:41 +0530

    Day2

--- WebApp/Migrations/20211126154459_AddCommandtoDB.Designer.cs
+++ WebApp/Migrations/20211126154459_AddCommandtoDB.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Models;
 
 namespace WebApp.Migrations
 {
     [DbContext(typeof(CommandContext))]
-    partial class CommandContextModelSnapshot : ModelSnapshot
+    [Migration("20211126154459_AddCommandtoDB")]
+    partial class AddCommandtoDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
